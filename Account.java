26dd40395ba6e92457080b826564097db6927c3c
Ben McMann <bbmcmann@volosys.com>
2020-01-28 08:25:04 -0700

    Fix Formatting

--- Account.java
+++ Account.java
@@ -13,50 +13,43 @@ public class Account
     private int accnum;  //The account number
     
     
-    public Account(int a)
-    {    
-	bal=0.0;
-	accnum=a;
+    public Account(int a){    
+	    bal=0.0;
+	    accnum=a;
     }
     
-    public void deposit(double sum)
-    {
-	if (sum>0)
-	    bal+=sum;    
-	else
-	    System.err.println("Account.deposit(...): "
-			       +"cannot deposit negative amount.");    
+    public void deposit(double sum){
+	    if (sum>0)
+	        bal+=sum;    
+	    else
+	        System.err.println("Account.deposit(...): "
+			        +"cannot deposit negative amount.");    
     }
     
-    public void withdraw(double sum)
-    {
-	if (sum>0)
-	    bal-=sum;    
-	else
-	    System.err.println("Account.withdraw(...): "
-			       +"cannot withdraw negative amount.");    
+    public void withdraw(double sum){
+	    if (sum>0)
+	        bal-=sum;    
+	    else
+	        System.err.println("Account.withdraw(...): "
+			        +"cannot withdraw negative amount.");    
     }
     
-    public double getBalance()
-    {
-	return bal;
+    public double getBalance(){
+	    return bal;
     }
     
-    public double getAccountNumber()
-    {
-	return accnum;
+    public double getAccountNumber(){
+	    return accnum;
     }
     
-    public String toString()
-    {
-	return "Acc " + accnum + ": " + "balance = " + bal;    
+    public String toString(){
+	    return "Acc " + accnum + ": " + "balance = " + bal;    
     }
     
-    public final void print()
-    {
-	//Don't override this,
-	//override the toString method
-	System.out.println( toString() );    
+    public final void print(){
+	    //Don't override this,
+	    //override the toString method
+	    System.out.println( toString() );    
     }
     
 }
