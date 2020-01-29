afa2f62f51f73758d90713ec26dcf476cf67589e
Ben McMann <bbmcmann@volosys.com>
2020-01-29 08:31:12 -0700

    Fin CurrentAccount

--- CurrentAccount.java
+++ CurrentAccount.java
@@ -1,10 +1,16 @@
 public class CurrentAccount extends Account{
 
-    public CurrentAccount(int a){
+    private double overdraftLim;
+    public CurrentAccount(int a, double overdraftLim){
         super(a);
-        
+        this.overdraftLim = overdraftLim;
     }
 
-
+    public void withdraw(double sum){
+        if(getBalance() - sum < overdraftLim){
+            System.out.println("Bruh, calm down, you are in overdraft. Get trashed");
+        } 
+        super.withdraw(sum); 
+    }
 
 }
