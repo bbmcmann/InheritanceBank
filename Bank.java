b05a4a53beedb01a0646076e6ccfd97b626502f0
Ben McMann <bbmcmann@volosys.com>
2020-01-29 09:20:53 -0700

    Need to fin bank

--- Bank.java
+++ Bank.java
@@ -1,9 +1,11 @@
 public class Bank{
 
     Account[] accounts;
+    double dividend;
 
-    public Bank(Account[] accounts){
+    public Bank(Account[] accounts, double dividend){
         this.accounts = accounts;
+        this.dividend = dividend;
     }
 
     public void update(){
@@ -15,6 +17,34 @@ public class Bank{
                 ((SavingsAccount) accounts[i]).update();
             }
         }
+    }
+
+    public void addAccount(Account newAccount){
+        Account[] temp = new Account[accounts.length+1];
+
+        for(int i = 0; i < accounts.length-1; i++){
+            temp[i] = accounts[i];
+        }
+        temp[temp.length-1] = newAccount;
+
+        accounts = temp;
+    }    
+
+    public void killAccount(int x){
+        Account[] temp = new Account[accounts.length-1];
+
+        for(int i = 0; i < accounts.length-1; i++){
+            if(i == x){
+
+            }
+            else{
+
+            }
+            temp[i] = accounts[i];
+        }
+        temp[temp.length-1] = newAccount;
+
+        accounts = temp;
     }
 
 }
